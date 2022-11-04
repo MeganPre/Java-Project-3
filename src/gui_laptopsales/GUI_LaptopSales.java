dd7e0f4765d773403fe13b680421bcd781e42d20
Megan Pretorius <10207@ctucareer.co.za>
2022-11-04 10:54:32 +0200

    Java Project 3

---
+++ src/gui_laptopsales/GUI_LaptopSales.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gui_laptopsales;
+
+/**
+ *
+ * @author megan
+ */
+public class GUI_LaptopSales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Welcome welc = new Welcome();
+        welc.setVisible(true);
+    }
+}
